010f5baa3d094cd9e0c640c89e6e3fb169d8ad86
Tatevik Santuryan <tateviksanturyan@gmail.com>
2023-03-20 04:03:21 -0800

    my first project

--- problem4.py
+++ problem4.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 from functools import  wraps
 import logging
-logging.basicConfig(filename='performance.log', format='%(asctime)s - %(funcName)s - %(message)s')
+logging.basicConfig(filename='performance.log', format='%(asctime)s - %(message)s', level=logging.INFO)
 
 def timer(func):
     @wraps(func)
     def wrapper(*args,**kwargs):
         start = datetime.now()
         res = func(*args,**kwargs)
-        logging.info(f"{datetime.now() - start}")
+        logging.info(f"{func.__name__,*args} - {datetime.now() - start}")
 
         return res
     return wrapper
