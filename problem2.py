b621eefa3e6f457f47f8c434b65ae27a2dfd070c
Tatevik Santuryan <tateviksanturyan@gmail.com>
2023-03-20 03:58:20 -0800

    my first homework

--- problem2.py
+++ problem2.py
@@ -1,28 +1,14 @@
+
 def indices_sum(nums, target):
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
-                return nums[i], nums[j]
+                return i, j
 
 
-nums = [1, 2, 3, 4, 5]
-target = 6
+nums = []
+target=42
 print(indices_sum(nums, target))
 
 
-def sumOfTwo(array, target):
-    i = 0
-    j = len(array) - 1
-    while i < j:
-      add = array[i] + array[j]
-      if add == target:
-        return array[i],array[j]
-      elif add < target:
-        i += 1
-      else:
-        j -= 1
-    return 'None'
 
-array = [12,15,10,2,4,7]
-target = 14
-print(sumOfTwo(array, target))
--- problem3.py
+++ problem3.py
@@ -5,4 +5,16 @@ def find_gcd(x, y):
 
     return x
 
-print(reduce(find_gcd, (15,25,35,45)))
+#print(reduce(find_gcd, (15,25,35,45)))
+
+
+def gcd(x, y, *args):
+    if len(args) > 0:
+        res = find_gcd(x,y)
+        for i in args:
+            res = find_gcd(res,i)
+        return res
+    else:
+        return find_gcd(x,y)
+
+print(gcd(25,35,45,55))
